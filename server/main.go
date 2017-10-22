d13bbe7f663a1485ab5e4c0f625e7d3cdd4ce89f
Americo Tomas <atomas@totallymoney.com>
2017-10-22 16:24:06 +0100

    Section5: 26. Write a go web server

---
+++ server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// fmt.Println("Hello in go")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":4000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from go")
+}
